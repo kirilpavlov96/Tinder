0e5e03ae255338c6fd5a053233456c5bf846403d
erolhadzhi <erol.hadzhi@gmail.com>
2016-03-24 09:27:50 +0200

    Added working logout servlet

--- Project/Tinder/src/main/java/controllers/Logout.java
+++ Project/Tinder/src/main/java/controllers/Logout.java
@@ -5,12 +5,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		HttpSession session = request.getSession(false);
+		if( session != null){
+			session.invalidate();
+		}
+		response.sendRedirect("/Tinder");
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
