31178fa244325d1a773ca260a03e1526f97e2724
Kiril <kirilpavlov96@gmail.com>
2016-03-26 15:45:16 +0200

    Photos of new candidate photos service

---
+++ Project/Tinder/src/main/java/controllers/LikeDislikeService.java
@@ -0,0 +1,103 @@
+package controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import exceptions.DBException;
+import exceptions.UnauthorizedException;
+import model.DAO.UserDAO;
+import model.POJO.User;
+
+public class LikeDislikeService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("./Home");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			response.setContentType("application/json");
+			User user = (User) request.getSession().getAttribute("user");
+			if (user == null) {
+				throw new UnauthorizedException("The user is not logged in.");
+			}
+
+			@SuppressWarnings("unchecked")
+			List<User> users = (List<User>) request.getSession().getAttribute("userCandidates");
+			if (users.size() == 0) {
+				addCandidates(request, users);
+				retrurnPhotosOfTheFirstUser(response, users);
+			}
+			else if(users.size()==1){
+				likeOrDislikeAndRemoveTheTopUser(request, user, users);
+				addCandidates(request, users);
+				retrurnPhotosOfTheFirstUser(response, users);
+			}
+			else{
+				likeOrDislikeAndRemoveTheTopUser(request, user, users);
+				retrurnPhotosOfTheFirstUser(response, users);
+			}
+
+		} catch (UnauthorizedException e) {
+			e.printStackTrace();
+			response.sendRedirect("./Home");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (DBException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void retrurnPhotosOfTheFirstUser(HttpServletResponse response, List<User> users)
+			throws DBException, IOException {
+		JSONObject json = new JSONObject();
+		if(users.size()==0){
+			json.put("photos", new LinkedList<String>().add("nousers.jpg"));
+		}
+		else{
+			json.put("photos", UserDAO.getAllPhotosOfUser(users.get(0).getUsername()));
+		}
+		PrintWriter pw = response.getWriter();
+		pw.print(json);
+		pw.flush();
+	}
+
+	private void likeOrDislikeAndRemoveTheTopUser(HttpServletRequest request, User user, List<User> users) {
+		if (((String) request.getAttribute("action")).equals("Like")) {
+			UserDAO.likeUser(user.getId(), users.get(0).getId());
+		}
+		if (((String) request.getAttribute("action")).equals("Dislike")) {
+			UserDAO.dislikeUser(user.getId(), users.get(0).getId());
+		}
+		users.remove(0);
+	}
+
+	private void addCandidates(HttpServletRequest request, List<User> users) {
+		List<User> newUsers = null;
+		try {
+			newUsers = UserDAO
+					.getFirstThreeNearbyUsers(((User) request.getSession().getAttribute("user")).getUsername());
+		} catch (DBException e) {
+			e.printStackTrace();
+		}
+		for (User u : newUsers) {
+			System.out.println(u);
+		}
+		users.addAll(newUsers);
+		request.getSession().setAttribute("userCandidates", users);
+	}
+
+}
