18a6308a6139eba32600850fd0c169ad85e3d454
Kiril <kirilpavlov96@gmail.com>
2016-03-27 16:13:15 +0300

    Discovery settings errors fixed

--- Project/Tinder/src/main/java/controllers/DiscoverySettings.java
+++ Project/Tinder/src/main/java/controllers/DiscoverySettings.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import exceptions.DBException;
 import model.DAO.UserDAO;
@@ -20,29 +21,30 @@ public class DiscoverySettings extends HttpServlet {
 		if (Home.checkValidSession(request, response)) {
 			request.getRequestDispatcher("WEB-INF/jsp/discovery-settings.jsp").forward(request, response);
 		}
-		else{
-			response.sendRedirect("./Home");
-		}
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		if (Home.checkValidSession(request, response)) {
+			HttpSession session = request.getSession(false);
 			int dist = Integer.parseInt(request.getParameter("search-distance"));
 			int minAge = Integer.parseInt(request.getParameter("age-range").trim().split(";")[0]);
 			int maxAge = Integer.parseInt(request.getParameter("age-range").trim().split(";")[1]);
-			boolean showFemale = request.getParameter("show-women").equals("on")?true:false;
-			boolean showMale = request.getParameter("show-men").equals("on")?true:false;
+			boolean showFemale = request.getParameter("show-women")==null?false:request.getParameter("show-women").equals("on");
+			boolean showMale = request.getParameter("show-men")==null?false:request.getParameter("show-men").equals("on");
 			try {
-				UserDAO.setUserDiscoverySettings(((User)request.getSession(false).getAttribute("user")).getId(),
+				UserDAO.setUserDiscoverySettings(((User)session.getAttribute("user")).getId(),
 						showMale, showFemale, dist, minAge, maxAge);
+				User user = UserDAO.getUser(((User)session.getAttribute("user")).getUsername());
+				session.setAttribute("user", user);
+				
 			} catch (DBException e) {
 				e.printStackTrace();
 			}
 			
 		}
-		doGet(request, response);
+		response.sendRedirect("./DiscoverySettings");
 	}
 
 }
