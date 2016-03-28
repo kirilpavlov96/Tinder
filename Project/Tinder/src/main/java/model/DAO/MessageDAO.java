22f2c83bd35919b6c279721782b234cbe7ba390d
erolhadzhi <erol.hadzhi@gmail.com>
2016-03-28 18:04:48 +0300

    Added servise for past messages + methods for it in MessageDAO

--- Project/Tinder/src/main/java/model/DAO/MessageDAO.java
+++ Project/Tinder/src/main/java/model/DAO/MessageDAO.java
@@ -2,21 +2,56 @@ package model.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;		
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import exceptions.DBException;
 import model.POJO.Chat;
+import model.POJO.Message;
 import model.POJO.User;
 
 public class MessageDAO {
-	private static final String FIND_CHAT_ID = "SELECT chat_id FROM"
-			+ " tinder.users_in_chats where user_id = ?"
-			+ " and chat_id in(SELECT chat_id FROM "
-			+ "tinder.users_in_chats where user_id = ?);";
-	private static final String SEND_MESSAGE = 
-			"insert into tinder.messages values(null,?,?,?);";
-	
-	public static void sendMessage(String msg,User from,User to) throws DBException{
+	private static final String FIND_CHAT_ID = "SELECT chat_id FROM" + " tinder.users_in_chats where user_id = ?"
+			+ " and chat_id in(SELECT chat_id FROM " + "tinder.users_in_chats where user_id = ?);";
+	private static final String SEND_MESSAGE = "insert into tinder.messages values(null,?,?,?);";
+	private static final String GET_MESSAGES_BEFORE_GIVEN_TIME = "SELECT * FROM tinder.messages where chat_id = ? and created_at < ? limit ?;";
+
+	public static List<Message> getLastMessagesFrom(int numMessages, User user1, User user2, LocalDateTime fromTime)
+			throws DBException {
+		List<Message> toReturn = new ArrayList<Message>(numMessages);
+		Connection conn = null;
+		PreparedStatement stmnt = null;
+		Chat chat = null;
+		ResultSet res = null;
+		try {
+			chat = new Chat(findChatId(user1, user2));
+			conn = ConnectionDispatcher.getConnection();
+			stmnt = conn.prepareStatement(GET_MESSAGES_BEFORE_GIVEN_TIME);
+			stmnt.setInt(1, chat.getId());
+			stmnt.setTimestamp(2, Timestamp.valueOf(fromTime));
+			stmnt.setInt(3, numMessages);
+			res = stmnt.executeQuery();
+			while(res.next()){
+				toReturn.add(new Message(res.getInt("id"), 
+						res.getInt("sender_id"),
+						res.getInt("chat_id"), 
+						res.getString("message"),
+						res.getTimestamp("created_at").toLocalDateTime()));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DBException("Something went wrong with the Database.", e);
+		} finally {
+			ConnectionDispatcher.returnConnection(conn);
+		}
+		return toReturn;
+	}
+
+	public static void sendMessage(String msg, User from, User to) throws DBException {
 		Connection conn = null;
 		PreparedStatement stmnt = null;
 		Chat chat = null;
@@ -36,7 +71,7 @@ public class MessageDAO {
 		}
 	}
 
-	public static int findChatId(User firstUser,User secondUser) throws DBException{
+	public static int findChatId(User firstUser, User secondUser) throws DBException {
 		Connection conn = null;
 		PreparedStatement stmnt = null;
 		ResultSet res = null;
@@ -46,12 +81,10 @@ public class MessageDAO {
 			stmnt.setInt(1, firstUser.getId());
 			stmnt.setInt(2, secondUser.getId());
 			res = stmnt.executeQuery();
-			if( res.next() ){
+			if (res.next()) {
 				return res.getInt(1);
-			}
-			else{
-				throw new DBException("Failed to find chat between users user1:"
-						+ firstUser + " user2:" + secondUser);
+			} else {
+				throw new DBException("Failed to find chat between users user1:" + firstUser + " user2:" + secondUser);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
